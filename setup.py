2838bd9fe45b4db8968685418f8033ca57780e4f
Nikita Tsvetkov <tsv1@fastmail.com>
2025-02-23 14:27:56 +0400

    Bump version → 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-shared-resource",
-    version="0.1.0",
+    version="0.2.0",
     description="A decorator for sharing costly resources between tests efficiently",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_shared_resource/__init__.py
+++ vedro_shared_resource/__init__.py
@@ -11,7 +11,7 @@ else:
 from async_lru import alru_cache
 
 __all__ = ("shared_resource",)
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 P = ParamSpec("P")
 R = TypeVar("R")
